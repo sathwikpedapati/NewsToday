6579707e9489ec88a1cb5555789591412d6d2952
sathwi_pedapati <161137317+sathwikpedapati@users.noreply.github.com>
2025-08-27 22:55:46 +0530

    Update NewsItem.js

--- client/src/Components/NewsItem.js
+++ client/src/Components/NewsItem.js
@@ -4,16 +4,19 @@ const NewsItem = ({ title, description, src, url }) => {
   const fallbackImage = "/news.jpg"; 
 
   return (
-    <div className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" style={{ maxWidth: "345px" }}>
+    <div 
+      className="card bg-dark text-light mb-3 d-inline-block my-3 mx-3 px-2 py-2" 
+      style={{ maxWidth: "345px" }}
+    >
       <img
-        src={src ? src : fallbackImage}
-        style={{ height: "150px", width: "330px" }}
+        src={src || fallbackImage}
+        style={{ height: "150px", width: "330px", objectFit: "cover" }}
         className="card-img-top"
-        alt="news"
+        alt={title || "news"}
       />
       <div className="card-body">
-        <h5 className="card-title">{title}</h5>
-        <p className="card-text">{description}</p>
+        <h5 className="card-title">{title || "No Title Available"}</h5>
+        <p className="card-text">{description || "No description available."}</p>
         <a href={url} className="btn btn-primary" target="_blank" rel="noreferrer">
           Read More
         </a>
